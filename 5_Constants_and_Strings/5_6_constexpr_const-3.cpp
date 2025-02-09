d8cc12319f894ad28292e606bcceab1583f41a60
pk0227 <myfriend0227@yahoo.com>
2025-02-09 14:56:56 +0530

    added constexpr examples

---
+++ 5_Constants_and_Strings/5_6_constexpr_const-3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int max(int x, int y) // this is a non-constexpr function
+{
+    if (x > y)
+        return x;
+    else
+        return y;
+}
+
+constexpr int cmax(int x, int y) // this is a constexpr function
+{
+    if (x > y)
+        return x;
+    else
+        return y;
+}
+
+int main()
+{
+    int m1 { max(5, 6) };            // ok
+    const int m2 { max(5, 6) };      // ok
+    //constexpr int m3 { max(5, 6) };  // compile error: max(5, 6) not a constant expression
+
+    int m4 { cmax(5, 6) };           // ok: may evaluate at compile-time or runtime
+    const int m5 { cmax(5, 6) };     // ok: may evaluate at compile-time or runtime
+    constexpr int m6 { cmax(5, 6) }; // okay: must evaluate at compile-time
+
+    return 0;
+}
